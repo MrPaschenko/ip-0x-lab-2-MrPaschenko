633427ee09a17c1722dbaa56a0dbe7d6d9cba1df
mrpaschenko <paschenkod2@gmail.com>
2022-12-01 14:34:35 +0100

    Add more tests

--- index.test.js
+++ index.test.js
@@ -13,7 +13,27 @@ test('Parse string', () => {
   expect(array).toEqual(['1', '2', '3', '+', '4', '5', '6', '=']);
 });
 
-test('Change calculator state', () => {
+test('Change calculator state (1)', () => {
+  const newCalculatorState = new CalculatorState(0, 0, '', true);
+
+  handleKeyPress(newCalculatorState, '1');
+  handleKeyPress(newCalculatorState, '2');
+  handleKeyPress(newCalculatorState, '6');
+  handleKeyPress(newCalculatorState, '+');
+  handleKeyPress(newCalculatorState, '3');
+  handleKeyPress(newCalculatorState, '2');
+  handleKeyPress(newCalculatorState, '7');
+  handleKeyPress(newCalculatorState, '=');
+
+  expect(newCalculatorState).toEqual({
+    screen: 453,
+    firstNumber: 126,
+    op: '+',
+    startSecondNumber: false
+  });
+});
+
+test('Change calculator state (2)', () => {
   const newCalculatorState = new CalculatorState(0, 0, '', true);
 
   handleKeyPress(newCalculatorState, '1');
@@ -29,9 +49,38 @@ test('Change calculator state', () => {
   });
 });
 
+test('Change calculator state (3)', () => {
+  const newCalculatorState = new CalculatorState(0, 0, '', true);
+
+  handleKeyPress(newCalculatorState, '5');
+  handleKeyPress(newCalculatorState, '7');
+  handleKeyPress(newCalculatorState, '+');
+  handleKeyPress(newCalculatorState, '1');
+  handleKeyPress(newCalculatorState, '1');
+  handleKeyPress(newCalculatorState, '1');
+  handleKeyPress(newCalculatorState, '=');
+
+  expect(newCalculatorState).toEqual({
+    screen: 168,
+    firstNumber: 57,
+    op: '+',
+    startSecondNumber: false
+  });
+});
+
 test('Calculate 1 + 1', () => {
   const stringArray = ['2', '+', '2', '='];
   expect(calculate(stringArray)).toEqual(4);
+});
+
+test('Calculate 7 * 8', () => {
+  const stringArray = ['7', '*', '8', '='];
+  expect(calculate(stringArray)).toEqual(56);
+});
+
+test('Calculate 152 + 52', () => {
+  const stringArray = ['1', '5', '2', '+', '5', '2', '='];
+  expect(calculate(stringArray)).toEqual(204);
 });
 
 test('Calculate 123 + 456', () => {
